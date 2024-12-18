a0c31248393ad2e25edf6e86cc37f7c78aebf1a5
Khant Naing Set <khantnaingset@onenex.co>
2024-12-19 03:34:02 +0700

    initial commit

---
+++ src/snowflake.ts
@@ -0,0 +1,143 @@
+/**
+ * Interface of a Snowflake after `Generator.deconstruct()`.
+ * @property {bigint} snowflake - Snowflake deconstructed from
+ * @property {bigint} timestamp - The timestamp the snowflake was generated
+ * @property {bigint} shard_id - The shard_id used when generating
+ * @property {bigint} increment - The increment of this snowflake
+ * @property {string} binary - The 64Bit snowflake binary string
+ * @interface DeconstructedSnowflake
+ */
+
+interface DeconstructedSnowflake {
+  timestamp: number;
+  shard_id: number;
+  sequence: number;
+  binary: string;
+}
+
+export class Snowflake {
+  /* c8 ignore start */
+  /**
+   * The generators epoch timestamp in milliseconds.
+   *
+   * Defaults to "1st of January, 2000, 00:00".
+   *
+   * @type {number}
+   */
+  /* c8 ignore end */
+
+  readonly EPOCH: number = Date.UTC(1970, 0, 1).valueOf();
+
+  /* c8 ignore start */
+  /**
+   * The id of the shard running this generator.
+   *
+   * Defaults to "1".
+   *
+   * @type {number}
+   */
+  /* c8 ignore end */
+
+  readonly SHARD_ID: number = 1;
+
+  /* c8 ignore start */
+  /**
+   * The sequence of the current running generator.
+   *
+   * Defaults to "1".
+   *
+   * @type {number}
+   */
+  /* c8 ignore end */
+  private SEQUENCE = 1;
+
+  /* c8 ignore start */
+  /**
+   * Generates a single snowflake.
+   * @param {Date|number} [timestamp = Date.now] - Timestamp to generate from
+   * @returns {bigint}
+   */
+  /* c8 ignore end */
+
+  /**
+   * Generates a single snowflake binary string.
+   */
+  private readonly cached64BitZeros =
+    '0000000000000000000000000000000000000000000000000000000000000000';
+
+  generate({
+    timestamp = Date.now(),
+    shard_id = this.SHARD_ID,
+  }: {
+    timestamp?: Date | number;
+    shard_id?: number;
+  } = {}): string {
+    if (timestamp instanceof Date) timestamp = timestamp.valueOf();
+    else timestamp = new Date(timestamp).valueOf();
+
+    let result = (BigInt(timestamp) - BigInt(this.EPOCH)) << BigInt(22);
+    result = result | (BigInt(shard_id % 1024) << BigInt(12));
+    result = result | BigInt(this.SEQUENCE++ % 4096);
+    return result.toString();
+  }
+
+  /**
+   * Deconstruct a snowflake to its values using the `Generator.epoch`.
+   * @param {string|string[]} snowflake - Snowflake(s) to deconstruct
+   * @returns {DeconstructedSnowflake|DeconstructedSnowflake[]}
+   */
+
+  parse(snowflake: string): DeconstructedSnowflake {
+    const binary = this.binary(snowflake);
+    return {
+      timestamp: this.extractBits(snowflake, 1, 41),
+      shard_id: this.extractBits(snowflake, 42, 10),
+      sequence: this.extractBits(snowflake, 52),
+      binary,
+    };
+  }
+
+  isValid(snowflake: string) {
+    if (snowflake.length !== 19 || !/^\d+$/.test(snowflake)) {
+      return false;
+    }
+    try {
+      this.parse(snowflake);
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+
+  /**
+   * Extract bits and their values from a snowflake.
+   * @param {string} snowflake - Snowflake to extract from
+   * @param {number|bigint} shift - Number of bits to shift before extracting
+   * @param {number|bigint} length - Number of bits to extract before stopping
+   * @returns {bigint}
+   */
+
+  extractBits(snowflake: string, start: number, length?: number): number {
+    return parseInt(
+      length
+        ? this.binary(snowflake).substring(start, start + length)
+        : this.binary(snowflake).substring(start),
+      2,
+    );
+  }
+
+  /**
+   * Transform a snowflake into its 64Bit binary string.
+   * @param {string} snowflake - Snowflake to transform
+   * @returns {string}
+   * @private
+   */
+
+  binary(snowflake: string): string {
+    const binValue = BigInt(snowflake).toString(2);
+    return binValue.length < 64
+      ? this.cached64BitZeros.substring(0, 64 - binValue.length) + binValue
+      : binValue;
+  }
+}
---
+++ test/snowflake.test.ts
@@ -0,0 +1,89 @@
+import { Snowflake } from '../src/index';
+
+const tests = [
+  {
+    timestamp: 1653653263221,
+    value: '6935924496540897281',
+    shard_id: 1,
+    sequence: 1,
+  },
+  {
+    timestamp: 1653653289600,
+    value: '6935924607185511855',
+    shard_id: 750,
+    sequence: 1455,
+  },
+  {
+    timestamp: 1653653311149,
+    value: '6935924697568571351',
+    shard_id: 750,
+    sequence: 4055,
+  },
+];
+
+let snowFlake: Snowflake;
+
+describe('index', () => {
+  beforeAll(() => {
+    snowFlake = new Snowflake();
+  });
+
+  describe('generate', () => {
+    it('should generate a snowflake', () => {
+      for (const test of tests) {
+        snowFlake.SEQUENCE = test.sequence;
+        expect(
+          snowFlake
+            .generate({
+              timestamp: test.timestamp,
+              shard_id: test.shard_id,
+            })
+            .toString(),
+        ).toEqual(test.value);
+      }
+    });
+    it('should generate a random snowflake', () => {
+      const generated: string[] = [];
+      Array(5)
+        .fill(null)
+        .map(() => {
+          generated.push(snowFlake.generate());
+        });
+      expect(generated.length).toEqual(new Set(generated).size);
+    });
+    it('should generate a unique snowflake', () => {
+      const generated: string[] = [];
+
+      Array(1e6)
+        .fill(null)
+        .map(() => {
+          generated.push(snowFlake.generate());
+        });
+      expect(generated.length).toEqual(new Set(generated).size);
+    });
+  });
+
+  describe('deconstruct', () => {
+    it('should deconstruct a snowflake', () => {
+      for (const test of tests) {
+        const parsed = snowFlake.parse(test.value);
+        expect(parsed.timestamp).toEqual(test.timestamp);
+        expect(parsed.sequence).toEqual(test.sequence);
+        expect(parsed.shard_id).toEqual(test.shard_id);
+      }
+    });
+  });
+
+  describe('isValid', () => {
+    it('should deconstruct a snowflake', () => {
+      const tests = [
+        { value: '6917082698162902015', valid: true },
+        { value: 'abv', valid: false },
+      ];
+      for (const test of tests) {
+        const parsed = snowFlake.isValid(test.value);
+        expect(parsed).toEqual(test.valid);
+      }
+    });
+  });
+});
